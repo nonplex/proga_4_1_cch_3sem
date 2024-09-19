cfba24ba60cfe29c696803de614748fe585ae316
nonplex <fuksvladislav@gmail.com>
2024-09-19 17:50:17 +0700

    Добавьте файлы проекта.

---
+++ proga_4_1_cch_3sem/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace proga_4_1_cch_3sem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textEdit1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Заменить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(595, 82);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(100, 20);
+            this.textEdit1.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 71F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 77F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 110F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 106F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(45, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.54348F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.45652F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(445, 222);
+            this.tableLayoutPanel1.TabIndex = 2;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 456);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textEdit1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ proga_4_1_cch_3sem/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace proga_4_1_cch_3sem
+{
+    public partial class Form1 : Form
+    {
+        // Переменная для хранения выбранной ячейки таблицы
+        private Point? selectedCell;
+        const int n = 5;
+        const double x = 5;
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+            // Создание таблицы 5x5
+            tableLayoutPanel1.RowCount = n; // Количество строк таблицы
+            tableLayoutPanel1.ColumnCount = n; // Количество столбцов таблицы
+            tableLayoutPanel1.AutoSize = false; // Отключаем автоматическое изменение размера таблицы
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 25)); // Устанавливаем ширину столбца в 25 пикселей
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 25)); // Устанавливаем высоту строки в 25 пикселей
+
+            // Заполнение таблицы значениями
+            FillTable(tableLayoutPanel1);
+
+            // Добавление таблицы на форму
+             // Закрепляем таблицу по краям формы
+            tableLayoutPanel1.AutoScroll = true; // Включаем автопрокрутку, если таблица не помещается на форму
+            Controls.Add(tableLayoutPanel1); // Добавляем таблицу на форму
+
+            // Подключение обработчиков событий
+            tableLayoutPanel1.MouseClick += tableLayoutPanel1_MouseDown; // Обработчик нажатия мыши на таблице
+
+            // Добавление кнопки для сохранения таблицы
+            button1.Click += button1_Click; // Обработчик нажатия кнопки
+        }
+
+        // Метод для заполнения таблицы значениями
+        private void FillTable(TableLayoutPanel table)
+        {
+            for (int j = 0; j < table.ColumnCount; j++) // Цикл по столбцам таблицы
+            {
+                for (int i = 0; i < table.RowCount; i++) // Цикл по строкам таблицы
+                {
+                    double value = Math.Pow(x, j + 1); // Вычисление значения ячейки таблицы
+                    TextBox textBox = new TextBox(); // Создание текстового поля для ячейки
+                    textBox.Text = value.ToString(); // Установка текста в поле
+                    textBox.ReadOnly = true; // Блокировка редактирования поля
+                    textBox.MouseDown += textBox_MouseDown; // Подключение обработчика нажатия мыши на поле
+                    table.Controls.Add(textBox, i, j); // Добавление поля в таблицу
+                }
+            }
+        }
+
+        // Обработчик нажатия кнопки "Сохранить"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (selectedCell.HasValue)
+            {
+                int column = selectedCell.Value.X;
+                int row = selectedCell.Value.Y;
+                Control control = tableLayoutPanel1.GetControlFromPosition(column, row);
+
+                if (control is TextBox textBox)
+                {
+                    if (double.TryParse(textEdit1.Text, out double value))
+                    {
+                        textBox.Text = value.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введенное значение не является числом.", "Неверный ввод", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Сначала выберите ячейку.", "Ячейка не выбрана", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // Обработчик нажатия мыши на текстовом поле ячейки таблицы
+        private void textBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender; // Получение текстового поля ячейки таблицы
+            textEdit1.Text = textBox.Text; // Установка текста из поля в textEdit1
+            int column = tableLayoutPanel1.GetColumn(textBox); // Номер столбца, в котором находится поле
+            int row = tableLayoutPanel1.GetRow(textBox); // Номер строки, в которой находится поле
+            selectedCell = new Point(column, row); // Сохранение выбранной ячейки таблицы
+        }
+
+        // Обработчик нажатия мыши на таблице (не используется в данном примере)
+        private void tableLayoutPanel1_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
+
+
